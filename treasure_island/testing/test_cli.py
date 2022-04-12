dcaac4f4ad5563891b4a4672d4335c5a226de8a1
Taras Novokhatskiy <taras.novokhatskiy@ringteam.com>
2022-04-12 04:14:33 +0300

    direct cli calls tests added

--- treasure_island/testing/test_cli.py
+++ treasure_island/testing/test_cli.py
@@ -1,10 +1,18 @@
 import os
 from unittest import TestCase
+from click.testing import CliRunner
+
+from treasure_island import cli
 
 
 class Test(TestCase):
     def test_clean_cache(self):
         assert os.system('rm-candles') == 0
+
+    def test_clean_cache_direct_call(self):
+        runner = CliRunner()
+        result = runner.invoke(cli.clean_candles_cache)
+        assert result.exit_code == 0
 
     def test_download_candles(self):
         pair = 'BTCUSDT'
@@ -13,4 +21,20 @@ class Test(TestCase):
         cmd = f'load-candles {pair} --from {from_str} -i {interval}'
 
         assert os.system(cmd) == 0
+
+    def test_download_candles_direct_call(self):
+        pair = 'BTCUSDT'
+        from_str = '2019-09-09'
+        interval = '4h'
+
+        runner = CliRunner()
+        result = runner.invoke(cli.download_candles, [
+            pair,
+            '--from_date', from_str,
+            '--interval', interval
+        ])
+
+        assert result.exit_code == 0
+
+
 
