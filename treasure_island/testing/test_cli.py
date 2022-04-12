b81f361e571e07e1c2aa01aac3750630e34d0d64
Taras Novokhatskiy <taras.novokhatskiy@ringteam.com>
2022-04-12 04:20:09 +0300

    minor refactor in tests

--- treasure_island/testing/test_cli.py
+++ treasure_island/testing/test_cli.py
@@ -5,14 +5,9 @@ from click.testing import CliRunner
 from treasure_island import cli
 
 
-class Test(TestCase):
+class TestSyscall(TestCase):
     def test_clean_cache(self):
         assert os.system('rm-candles') == 0
-
-    def test_clean_cache_direct_call(self):
-        runner = CliRunner()
-        result = runner.invoke(cli.clean_candles_cache)
-        assert result.exit_code == 0
 
     def test_download_candles(self):
         pair = 'BTCUSDT'
@@ -22,7 +17,14 @@ class Test(TestCase):
 
         assert os.system(cmd) == 0
 
-    def test_download_candles_direct_call(self):
+
+class TestDirectCall(TestCase):
+    def test_clean_cache(self):
+        runner = CliRunner()
+        result = runner.invoke(cli.clean_candles_cache)
+        assert result.exit_code == 0
+
+    def test_download_candles(self):
         pair = 'BTCUSDT'
         from_str = '2019-09-09'
         interval = '4h'
