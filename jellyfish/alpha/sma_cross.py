6e632067504ab4c0e8e9a58bc1bae86b4014736a
magicPony <novohatskiy.taras@gmail.com>
2022-04-19 22:16:39 +0300

    Core structure refactor (#14)

--- jellyfish/alpha/sma_cross.py
+++ jellyfish/alpha/sma_cross.py
@@ -1,35 +1,10 @@
-# pylint: disable=W0201
-
 """
-Strategies list
+Simple moving average crossover strategy
 """
-
-from backtesting import Strategy
-from backtesting.test import SMA
 from backtesting.lib import crossover
+from backtesting.test import SMA
 
-
-class DummyStrategy(Strategy):
-    """
-    Dummy empty strategy
-    """
-    def init(self):
-        pass
-
-    def next(self):
-        pass
-
-    def mark_as_indicator(self, column_name, overlay=None, name=None, scatter=False):
-        """
-        Mark dataframe column as an indicator
-        Args:
-            column_name: dataframe column namae
-            overlay: plot indicator overlay candles chart
-            name: indicator name
-            scatter: plot circles instead of connected line segments
-        """
-        name = name or column_name
-        self.I(lambda x: x, self.data.df[column_name], name=name, overlay=overlay, scatter=scatter)
+from jellyfish.core import Strategy
 
 
 class SmaCross(Strategy):
