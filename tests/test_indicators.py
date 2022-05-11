1ef5cd3daf18fcb9e2037f31b316fe4555cfa7cf
Taras Novokhatskiy <taras.novokhatskiy@ringteam.com>
2022-05-12 00:17:24 +0300

    refactor tests

--- tests/test_indicators.py
+++ tests/test_indicators.py
@@ -3,45 +3,18 @@ from unittest import TestCase
 
 from jellyfish import indicator
 from jellyfish.candles_loader import load_candles_history
-from jellyfish.core import Backtest, Strategy, Client
+from jellyfish.core import Client, Strategy, Backtest
 
 
-class DummyStrategyWithIndicators(Strategy):
+class DummyWithIndicators(Strategy):
     def init(self):
-        self.I(indicator.dumb_sr_lines, self.data.High, self.data.Low, name='S/R line',
-               overlay=True)
-        self.I(indicator.wad, self.data.High, self.data.Low, self.data.Close,
-               name='Williams Accumulation/Distribution')
-        self.I(indicator.will_r, self.data.High, self.data.Low, self.data.Close,
-               name='Williams %R')
-        self.I(indicator.wilders, self.data.Close, 5, name='Wilders Smoothing [fast]',
-               overlay=True)
-        self.I(indicator.wilders, self.data.Close, 20, name='Wilders Smoothing [slow]',
-               overlay=True)
-        self.I(indicator.wilders, self.data.Close, 40, name='Wilders Smoothing [very slow]',
-               overlay=True)
-        self.I(indicator.stoch_rsi, self.data.Close, name='Stochastic RSI')
-        self.I(indicator.stoch, self.data.High, self.data.Low, self.data.Close,
-               name='Stochastic Oscillator')
+        super(DummyWithIndicators, self).init()
+        self.I(indicator.dumb_sr_lines, self.data.High, self.data.Low)
+        self.I(indicator.stoch, self.data.High, self.data.Low, self.data.Close)
         self.I(indicator.aroon, self.data.High, self.data.Low)
-        self.I(indicator.aroon_oscillator, self.data.High, self.data.Low, name='Aroon Oscillator')
-        self.I(indicator.fisher, self.data.High, self.data.Low, name='Fished')
-        self.I(indicator.cmo, self.data.Close, name='Chande Momentum Oscillator')
-        self.I(indicator.bop, self.data.Open, self.data.High, self.data.Low,
-               self.data.Close, name='Balance of Power')
-        self.I(indicator.dpo, self.data.Close, name='Detrended Price Oscillator')
-        self.I(indicator.kvo, self.data.High, self.data.Low, self.data.Close, self.data.Volume,
-               15, 25, name='Klinger Volume Oscillator')
-        self.I(indicator.lag, self.data.Close, name='Lag', overlay=True)
-        self.I(indicator.mass, self.data.High, self.data.Low, name='Mass')
-        self.I(indicator.rsi, self.data.Close, 20)
-        self.I(indicator.bollinger_bands, self.data.df, 10, 1.5, name='Bollinger bands')
-        self.I(indicator.ema, self.data.Close, 20, 1.5, name='EMA')
-        self.I(indicator.sma, self.data.Close, 20, name='SMA', overlay=True)
+        self.I(indicator.fisher, self.data.High, self.data.Low)
+        self.I(indicator.bollinger_bands, self.data.df, 10, 1.5)
         self.I(indicator.macd, self.data.Close, 12, 26, 9)
-        self.I(indicator.awesome, self.data.High, self.data.Low, name='Awesome')
-
-        Strategy.init(self)
 
 
 class Test(TestCase):
@@ -52,9 +25,26 @@ class Test(TestCase):
 
         frame['i_zigzag'] = indicator.zigzag(frame.Close.to_numpy(), 2e-2)
         frame['i_hurst_random_walk'] = indicator.hurst(frame.Close.to_numpy())
-        frame['i_hurst_change'] = indicator.hurst(frame.Close.to_numpy(), 100, 'change')
-        frame['i_hurst_price'] = indicator.hurst(frame.Close.to_numpy(), 200, 'price')
+        frame['i_hurst_change'] = indicator.hurst(frame.Close.to_numpy(), 100,
+                                                  indicator.HURST_CHANGE)
+        frame['i_hurst_price'] = indicator.hurst(frame.Close.to_numpy(), 200, indicator.HURST_PRICE)
+        frame['i_wad'] = indicator.wad(frame.High, frame.Low, frame.Close)
+        frame['i_will_r'] = indicator.will_r(frame.High, frame.Low, frame.Close)
+        frame['i_wilders_fast'] = indicator.wilders(frame.Close, 5)
+        frame['i_wilders_slow'] = indicator.wilders(frame.Close, 20)
+        frame['i_wilders_very_slow'] = indicator.wilders(frame. Close, 40)
+        frame['i_stoch_rsi'] = indicator.stoch_rsi(frame.Close)
+        frame['i_aroon_osc'] = indicator.aroon_oscillator(frame.High, frame.Low)
+        frame['i_cmo'] = indicator.cmo(frame.Close)
+        frame['i_bop'] = indicator.bop(frame.Open, frame.High, frame.Low, frame.Close)
+        frame['i_dpo'] = indicator.dpo(frame.Close)
+        frame['i_kvo'] = indicator.kvo(frame.High, frame.Low, frame.Close, frame.Volume, 15, 25)
+        frame['i_mass'] = indicator.mass(frame.High, frame.Low)
+        frame['i_rsi'] = indicator.rsi(frame.Close, 20)
+        frame['i_ema'] = indicator.ema(frame.Close, 20, 1.5)
+        frame['i_sma'] = indicator.sma(frame.Close, 20)
+        frame['i_awesome'] = indicator.awesome(frame.High, frame.Low)
 
-        backtest = Backtest(frame.reset_index(), DummyStrategyWithIndicators)
+        backtest = Backtest(frame.reset_index(), DummyWithIndicators)
         backtest.run()
         backtest.plot()
