8821508b91e676e5749a649e6c3764cbf3570984
Taras Novokhatskiy <taras.novokhatskiy@ringteam.com>
2022-05-16 18:11:04 +0300

    stop all test added

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -7,19 +7,24 @@ from jellyfish import cli
 
 
 class TestCrawler(TestCase):
-    def test_wrong_argument(self):
-        runner = CliRunner()
+    def __init__(self, *args, **kwargs):
+        super(TestCrawler, self).__init__(*args, **kwargs)
+        self.runner = CliRunner()
 
+    def test_call_with_no_args(self):
+        self.runner.invoke(cli.crawler_cli, [])
+
+    def test_wrong_argument(self):
         pair = 'bchbtc'
 
-        result = runner.invoke(cli.crawler_cli, [
+        result = self.runner.invoke(cli.crawler_cli, [
             '--start',
             pair,
             '--ttl', '1qq',
             '--block'])
         self.assertNotEqual(result.exit_code, 0)
 
-        result = runner.invoke(cli.crawler_cli, [
+        result = self.runner.invoke(cli.crawler_cli, [
             '--start',
             pair,
             '--period', '1q',
@@ -27,34 +32,41 @@ class TestCrawler(TestCase):
         self.assertNotEqual(result.exit_code, 0)
 
     def test_start_stop(self):
-        runner = CliRunner()
-
         pair = 'bchbtc'
+        pair1 = 'bchusdt'
         period = '1s'
         ttl = '10s'
 
-        result = runner.invoke(cli.crawler_cli, ['--status'])
+        result = self.runner.invoke(cli.crawler_cli, ['--status'])
         self.assertEqual(result.exit_code, 0)
-        assert result.output.strip() == 'There is no active sessions.'
+        self.assertEqual(result.output.strip(), 'There is no active sessions.')
 
-        result = runner.invoke(cli.crawler_cli, [
+        result = self.runner.invoke(cli.crawler_cli, [
             '--start',
-            pair,
+            pair, pair1,
             '--period', period,
             '--ttl', ttl,
             '--block'])
         self.assertEqual(result.exit_code, 0)
 
-        result = runner.invoke(cli.crawler_cli, [
+        result = self.runner.invoke(cli.crawler_cli, [
             '--status',
             pair])
         self.assertEqual(result.exit_code, 0)
-        assert result.output.strip() == f'Active sessions list: {pair.upper()}'
+        self.assertTrue(pair.upper() in result.output)
+        self.assertTrue(pair1.upper() in result.output)
 
         # TODO: CliRunner captures io streams which causes problems to service management
-        time.sleep(4)
-        result = runner.invoke(cli.crawler_cli, ['--stop', pair])
+        time.sleep(3)
+        result = self.runner.invoke(cli.crawler_cli, ['--stop', pair])
         self.assertEqual(result.exit_code, 0)
+
+        result = self.runner.invoke(cli.crawler_cli, ['--stop'])
+        self.assertEqual(result.exit_code, 0)
+
+        result = self.runner.invoke(cli.crawler_cli, ['--status'])
+        self.assertEqual(result.exit_code, 0)
+        self.assertEqual(result.output.strip(), 'There is no active sessions.')
 
 
 class TestCandlesLoading(TestCase):
