fec772d43b1a5bae0caeebfe6ea008009f0e75f1
magicPony <novohatskiy.taras@gmail.com>
2022-05-18 21:43:04 +0300

    fix updates regressions (#34)

--- jellyfish/utils.py
+++ jellyfish/utils.py
@@ -8,17 +8,18 @@ import pandas as pd
 from jellyfish.core import Strategy, Backtest
 
 
-def plot_ohlc(ohlc: pd.DataFrame, open_browser=True, show_legend=True):
+def plot_ohlc(ohlc: pd.DataFrame, open_browser=True, show_legend=True, plot_volume=True):
     """
     Plot OHLC chart from dataframe
     Args:
         ohlc: dataframe
         open_browser: open html report in browser
         show_legend: show equity/pnl legend
+        plot_volume: plot volume chart
     """
     backtest = Backtest(ohlc, strategy=Strategy)
     backtest.run()
-    backtest.plot(open_browser=open_browser, show_legend=show_legend)
+    backtest.plot(open_browser=open_browser, show_legend=show_legend, plot_volume=plot_volume)
 
 
 def disable_warnings():
