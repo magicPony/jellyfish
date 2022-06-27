a671e0d42df7c00549d488238272fdf8975fc3fc
Taras Novokhatskyi <novohatskiy.taras@gmail.com>
2022-06-28 00:18:12 +0300

    update tests

--- jellyfish/history_loader/__init__.py
+++ jellyfish/history_loader/__init__.py
@@ -1,5 +1,5 @@
 """
 History data management module
 """
-from jellyfish.history_loader.candles import (load_candles_history, get_sample_frame, clean_candles_cache)
+from jellyfish.history_loader.candles_old import load_candles_history
 from jellyfish.history_loader.orderbook import load_orderbook_history
--- jellyfish/history_loader/candles.py
+++ jellyfish/history_loader/candles.py
@@ -1,117 +1,75 @@
-"""
-Candles history manager module that is responsive for candles downloading/saving/loading from cache
-"""
-import logging
-import math
+import sqlite3
 from datetime import datetime, timedelta
-from typing import Union
+from enum import Enum
+from functools import lru_cache
 
 import numpy as np
 import pandas as pd
-from tqdm.auto import tqdm
 from unicorn_binance_rest_api.helpers import interval_to_milliseconds
 
-from jellyfish.constants import (CANDLES_HISTORY_PATH, DATE, OPEN, HIGH, LOW, CLOSE, VOLUME,
-                                 QUOTE_ASSET_VOLUME, NUM_OF_TRADES, TAKER_BUY_ASSET_VOLUME,
-                                 TAKER_SELL_ASSET_VOLUME)
+from jellyfish.constants import CANDLES_DB_PATH
 from jellyfish.core import Client
 from jellyfish.history_loader.orderbook import load_orderbook_history
 
 CANDLES_IN_CHUNK = 1000
 
-
-def clean_candles_cache():
-    """
-    Cleans candles cache data
-    """
-    CANDLES_HISTORY_PATH.mkdir(exist_ok=True, parents=True)
-    for cached_file_path in CANDLES_HISTORY_PATH.iterdir():
-        cached_file_path.unlink()
+TIMESTAMP = 'timestamp'
 
 
-def binance_response_to_dataframe(candles) -> pd.DataFrame:
-    """
-    Transform binance response to dataframe
-    :param candles: candles sequence
-    :return: candles dataframe
-    """
-
-    def to_numbers(seq: list):
-        return [int(i) if isinstance(i, int) else float(i) for i in seq]
-
-    def to_datetime(timestamp):
-        return datetime.fromtimestamp(timestamp // 1e3).strftime('%Y-%m-%d %H:%M')
-
-    candles = [to_numbers(i) for i in candles]
-    candles = np.array(candles)
-    candles = pd.DataFrame({
-        DATE: candles[:, 6],
-        OPEN: candles[:, 1],
-        HIGH: candles[:, 2],
-        LOW: candles[:, 3],
-        CLOSE: candles[:, 4],
-        VOLUME: candles[:, 5],
-        QUOTE_ASSET_VOLUME: candles[:, 7],
-        NUM_OF_TRADES: candles[:, 8],
-        TAKER_BUY_ASSET_VOLUME: candles[:, 9],
-        TAKER_SELL_ASSET_VOLUME: candles[:, 10],
-    })
-
-    candles.Date = candles.Date.apply(to_datetime)
-    candles.Date = pd.to_datetime(candles.Date)
-    candles.set_index('Date', inplace=True)
-
-    return candles
+class ResponseDTypes(Enum):
+    REAL = 'REAL'
+    INT = 'INT'
 
 
-def read_candles_frame(frame_path):
-    """
-    Read dataframe with candles
-    """
-    frame = pd.read_csv(frame_path, index_col='Date', parse_dates=True, infer_datetime_format=True)
-    frame.index.names = ['Date']
-    return frame
+RESPONSE_STRUCTURE = {
+    'open': ResponseDTypes.REAL,
+    'high': ResponseDTypes.REAL,
+    'low': ResponseDTypes.REAL,
+    'close': ResponseDTypes.REAL,
+    'volume': ResponseDTypes.REAL,
+    TIMESTAMP: ResponseDTypes.INT,
+    'quote_asset_volume': ResponseDTypes.REAL,
+    'num_of_trades': ResponseDTypes.INT,
+    'taker_buy_asset_volume': ResponseDTypes.REAL,
+    'taker_sell_asset_volume': ResponseDTypes.REAL
+}
 
 
-def get_sample_frame():
-    """
-    Get random candles dataframe from cache if possible
-    """
-    CANDLES_HISTORY_PATH.mkdir(exist_ok=True, parents=True)
-    for sample_path in CANDLES_HISTORY_PATH.iterdir():
-        return read_candles_frame(sample_path)
+def load_from_db(cursor: sqlite3.Cursor,
+                 table_name,
+                 start_dt: datetime,
+                 end_dt: datetime,
+                 interval_ms: int):
+    microseconds_in_millisecond = timedelta(milliseconds=1) / timedelta(microseconds=1)
 
-    return None
+    first_timestamp = start_dt.timestamp() * microseconds_in_millisecond
+    first_timestamp = first_timestamp + (interval_ms - first_timestamp % interval_ms) % interval_ms
+    last_timestamp = end_dt.timestamp() * microseconds_in_millisecond
 
-
-def _load_candles_chunk(
-        client: Union[None, Client],
-        pair_sym: str,
-        start_dt: datetime,
-        end_dt: datetime,
-        interval: str) -> pd.DataFrame:
-    """
-    Loads single chunk of candles history
-    :param client: RestManager Binance client
-    :param pair_sym: trading pair
-    :param start_dt: start date
-    :param end_dt: end date
-    :param interval: candle interval
-    :return: candles history chunk dataframe
-    """
-    cache_path = CANDLES_HISTORY_PATH / f'{pair_sym}_{interval}_{start_dt}_{end_dt}.csv'
-    if cache_path.exists():
-        logging.debug('Loading candles history from cache')
-        return read_candles_frame(cache_path)
-
-    logging.debug('Downloading candles history from Binance')
-    candles = client.get_historical_klines(pair_sym, interval, str(start_dt), str(end_dt))
-    frame = binance_response_to_dataframe(candles)
-    cache_path.parent.mkdir(exist_ok=True, parents=True)
-    frame.to_csv(cache_path)
-    return frame
+    cmd = f'SELECT * FROM {table_name} WHERE {first_timestamp}<={TIMESTAMP} AND {TIMESTAMP}<={last_timestamp}'
+    return list(cursor.execute(cmd))
 
 
+def insert_to_db(cursor: sqlite3.Cursor,
+                 table_name,
+                 candles: list[list]):
+    cmd = f'INSERT OR IGNORE INTO {table_name} ' \
+          f'VALUES ({",".join("?" for _ in range(np.shape(candles)[1]))})'
+    cursor.executemany(cmd, candles)
+
+
+def create_db_connection(table_name):
+    connection = sqlite3.connect(CANDLES_DB_PATH.as_posix())
+    cursor = connection.cursor()
+    cmd = f'CREATE TABLE IF NOT EXISTS {table_name} ' \
+          f'({",".join(f"{k} {v.value}" for k, v in RESPONSE_STRUCTURE.items())},' \
+          f'PRIMARY KEY({TIMESTAMP}))'
+    cursor.execute(cmd)
+
+    return connection
+
+
+@lru_cache(maxsize=20)
 def load_candles_history(
         pair_sym: str,
         start_dt: datetime = None,
@@ -120,20 +78,9 @@ def load_candles_history(
         *,
         client: Client = None,
         candles_num=None,
-        read_orderbook=False) -> pd.DataFrame:
-    """
-    Downloads japanese candles from binance with cached data usage if possible
-    :param pair_sym: trading pair
-    :param start_dt: start date
-    :param end_dt: end date
-    :param interval: candle interval
-    :param client: binance client
-    :param candles_num: define number of candles to load
-    :param read_orderbook: read orderbook data flag
-    :return: candles dataframe
-    """
-    client = client or Client()
+        read_orderbook=False):
     assert start_dt is not None or end_dt is not None
+    interval_ms = interval_to_milliseconds(interval)
     if candles_num is not None:
         duration = timedelta(milliseconds=interval_to_milliseconds(interval) * candles_num)
         if start_dt is not None:
@@ -141,23 +88,31 @@ def load_candles_history(
         else:
             start_dt = end_dt - duration
 
-    interval_ms = interval_to_milliseconds(interval)
-    total_candles = (end_dt - start_dt) / timedelta(milliseconds=interval_ms)
-    chunks_num = math.ceil(total_candles / CANDLES_IN_CHUNK)
-    dates = [(start_dt + timedelta(milliseconds=i * interval_ms * CANDLES_IN_CHUNK))
-             for i in range(chunks_num)] + [end_dt]
+    else:
+        candles_num = (end_dt - start_dt) // timedelta(milliseconds=interval_ms)
 
-    result = []
-    for chunk_start_dt, chunk_end_dt in tqdm(list(zip(dates[:-1], dates[1:]))):
-        candles = _load_candles_chunk(client, pair_sym.upper(), chunk_start_dt, chunk_end_dt,
-                                      interval)
-        result.append(candles)
+    table_name = pair_sym + interval
+    connection = create_db_connection(table_name)
+    cursor = connection.cursor()
 
-    result = pd.concat(result).sort_index().sort_index(axis=1)
+    candles = load_from_db(cursor, table_name, start_dt, end_dt, interval_ms)
+    if len(candles) < candles_num - 3:  # (\/)(*--*)(\/)
+        client = client or Client()
+
+        binance_response = client.get_historical_klines(pair_sym, interval, str(start_dt), str(end_dt))
+        candles = [candle[1:11] for candle in binance_response]
+
+        insert_to_db(cursor, table_name, candles)
+        connection.commit()
+
+    candles = np.array(candles)
+    df = pd.DataFrame()
+    for i, (key, val_type) in enumerate(RESPONSE_STRUCTURE.items()):
+        col_name = ''.join(word.capitalize() for word in key.split('_'))
+        df[col_name] = candles[:, i].astype(np.int if val_type == ResponseDTypes.INT else np.float)
+
     if read_orderbook:
-        orderbook = load_orderbook_history(pair_sym, result.index,
-                                           max_lag=timedelta(milliseconds=interval_ms))
-        result = result.join(orderbook)
+        orderbook = load_orderbook_history(pair_sym, df.index, max_lag=timedelta(milliseconds=interval_ms))
+        df = df.join(orderbook)
 
-    result.index.name = DATE
-    return result
+    return df
